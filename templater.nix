115037e8cfd150ebef12547f64da0f2bc18be46d
SimonReilich <accounts@sreilich.de>
2026-05-21 11:55:54 +0200

    updated templater

--- templater.nix
+++ templater.nix
@@ -6,22 +6,22 @@
 
 pkgs.stdenv.mkDerivation rec {
   pname = "obsidian.plugins.templater";
-  version = "2.20.4";
+  version = "2.20.5";
   repo = "https://github.com/SilentVoid13/Templater";
 
   mainJs = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/main.js";
-    sha256 = "sha256-irKJWK2OJfgaTCx8HUDx69wF10IAb/zxxbmIl5+6Alk=";
+    sha256 = "sha256-gcjQuyEsnBJpkC7O7kuqIcg7qF1tjfo0ioBBa8csXuw=";
   };
 
   manifest = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/manifest.json";
-    sha256 = "sha256-DYIanxAkKfg4NLf/lKgLmdcAdS1c8VJkRyZBbJmjGHE=";
+    sha256 = "sha256-XfFFHinKveykRd3zRuUfrAqT8Ae/D40QCSTTXXo6A9g=";
   };
 
   stylesCss = pkgs.fetchurl {
     url = "${repo}/releases/download/${version}/manifest.json";
-    sha256 = "sha256-DYIanxAkKfg4NLf/lKgLmdcAdS1c8VJkRyZBbJmjGHE=";
+    sha256 = "sha256-XfFFHinKveykRd3zRuUfrAqT8Ae/D40QCSTTXXo6A9g=";
   };
 
   phases = [ "installPhase" ];
